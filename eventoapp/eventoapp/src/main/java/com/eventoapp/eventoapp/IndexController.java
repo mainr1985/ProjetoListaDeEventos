87001eb15312d53ac56b45423c8b7dc31676c38e
Maíra N. Rocha <mairanrocha@hotmail.com>
2023-09-21 21:35:10 -0300

    Hello World Spring Boot

---
+++ eventoapp/eventoapp/src/main/java/com/eventoapp/eventoapp/IndexController.java
@@ -0,0 +1,15 @@
+//hello world!
+package com.eventoapp.eventoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	//quando receber a requisição http contendo a "/", exibe a view que está criada em resources->templates. 
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
